471664dcdba2259505aac202d319b119094c3be7
jasenmichael <jasen@jasenmichael.com>
2023-02-02 21:25:14 -0600

    chore: move getTokens to a function

--- app.js
+++ app.js
@@ -14,15 +14,29 @@ const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
 const redirect_uri = 'http://localhost:8080'
 
-// Route for getting authentication endpoint
+// ROUTES
+// - Route for getting authentication endpoint
 app.get('/get-intuit-client-id', (req, res) => {
   res.send({ client_id })
 })
 
-// Route for setting tokens, requires authorization code (generated after authentication to the auth endpoint)
+// - Route for setting tokens, requires authorization code (generated after authentication to the auth endpoint)
 app.post('/set-intuit-tokens', async (req, res) => {
   const code = req.body.code
   console.log('authorization code', code)
+  !code && res.status(400).send({ error: 'Missing authorization code' })
+  try {
+    const tokens = await getTokens(code)
+    // save tokens to db here
+    res.send({ tokens })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+})
+// END ROUTES
+
+// Function for getting tokens from Intuit
+const getTokens = async (code) => {
   const url = 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer'
   const body = `grant_type=authorization_code&code=${code}&redirect_uri=${redirect_uri}&client_id=${client_id}&client_secret=${client_secret}`
   const { data } = await axios.post(url, body, { 
@@ -31,13 +45,8 @@ app.post('/set-intuit-tokens', async (req, res) => {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   })
-  if (data.error) {
-    res.send({ error_description: data.error.error_description, error: data.error.error })
-  }
-  console.log(data)
-  // save to db here
-  res.send({ tokens: data })
-})
+  return data
+}
 
 // Start the server
 app.listen(3000, () => console.log('Server started on port 3000'))
